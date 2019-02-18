455018ee6c2fecf220a6487ee0c129e61c57cd7c
Madeleine Woodbury <randine25@gmail.com>
2019-02-18 16:09:50 -0500

    Edit Post Image Implemented

--- routes/upload.js
+++ routes/upload.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const { ensureAuthenticated } = require("../helpers/auth");
+const Categories = require("../models/Category");
 const cloudinary = require("cloudinary");
 // const cloudinaryStorage = require("multer-storage-cloudinary");
 const multer = require("multer");
@@ -99,6 +100,36 @@ router.post(
           req.flash("error_msg", "Something went wrong");
           return res.redirect("/");
         });
+    });
+  }
+);
+
+// Edit Post Image Process Route
+router.put(
+  "/edit/post/image/:id",
+  ensureAuthenticated,
+  upload.single("image"),
+  (req, res) => {
+    Post.findOne({ _id: req.params.id }).then(post => {
+      if (!req.file) {
+        return res.render("posts/edit", { post: post, Categories: Categories });
+      } else {
+        cloudinary.uploader.upload(req.file.path, result => {
+          req.body.image = result.secure_url;
+          // New Image Value
+          post.image = req.body.image;
+
+          post
+            .save()
+            .then(post => {
+              res.render("posts/edit", { post: post, Categories: Categories });
+            })
+            .catch(err => {
+              req.flash("error_msg", "Something went wrong");
+              return res.redirect("/");
+            });
+        });
+      }
     });
   }
 );
