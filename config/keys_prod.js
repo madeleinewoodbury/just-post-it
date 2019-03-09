d2768de9d04daf9b823c5a9471f50d9974d128dc
Madeleine Woodbury <randine25@gmail.com>
2019-03-09 18:51:34 -0500

    config file updated

---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  cloudName: process.env.CLOUD_NAME,
+  cloudinaryApiKey: process.env.API_KEY,
+  cloudinaryApiSecret: process.env.API_SECRET
+};
